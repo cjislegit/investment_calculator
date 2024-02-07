d0d512413def873a1b0008aeddbc4be09c7f0b69
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-02-06 21:12:19 -0800

    Started working on the user input

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,16 @@
+const UserInput = () => {
+  return (
+    <div id='user-input'>
+      <div className='input-group'>
+        <label htmlFor=''>Initial Investment</label>
+        <input type='text' value='' />
+      </div>
+      <div className='input-group'>
+        <label htmlFor=''>Initial Investment</label>
+        <input type='text' value='' />
+      </div>
+    </div>
+  );
+};
+
+export default UserInput;
