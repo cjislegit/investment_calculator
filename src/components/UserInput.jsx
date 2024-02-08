f82a452de31ec1071913e8c11fa60f3f1589d8e8
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-02-07 16:21:29 -0800

    Added id to input fields

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -3,22 +3,22 @@ const UserInput = () => {
     <section id='user-input'>
       <div className='input-group'>
         <p>
-          <label htmlFor=''>Initial Investment</label>
-          <input type='number' value='' required />
+          <label htmlFor='initialInvestment'>Initial Investment</label>
+          <input id='initialInvestment' type='number' value='' required />
         </p>
         <p>
-          <label htmlFor=''>Annual Investment</label>
-          <input type='number' value='' required />
+          <label htmlFor='annualInvestment'>Annual Investment</label>
+          <input id='annualInvestment' type='number' value='' required />
         </p>
       </div>
       <div className='input-group'>
         <p>
-          <label htmlFor=''>Expected Return</label>
-          <input type='number' value='' required />
+          <label htmlFor='expectedReturn'>Expected Return</label>
+          <input id='expectedReturn' type='number' value='' required />
         </p>
         <p>
-          <label htmlFor=''>Duration</label>
-          <input type='number' value='' required />
+          <label htmlFor='duration'>Duration</label>
+          <input id='duration' type='number' value='' required />
         </p>
       </div>
     </section>
