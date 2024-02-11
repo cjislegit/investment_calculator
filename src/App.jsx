3962c23550d84d904dafb285e18ba2c88ce10505
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-02-10 21:33:37 -0800

    Added two way binding for userinput

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import UserInput from './components/UserInput';
 
 function App() {
+  let [values, setValues] = useState({
+    initialInvestment: '',
+    annualInvestment: '',
+    expectedReturn: '',
+    duration: '',
+  });
+
+  const handleChangeValues = (event) => {
+    let id = event.target.id;
+    let value = event.target.value;
+
+    setValues((prevValue) => {
+      let newValues = { ...prevValue, [id]: value };
+      return newValues;
+    });
+  };
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput values={values} handleChangeValues={handleChangeValues} />
     </>
   );
 }
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,24 +1,50 @@
-const UserInput = () => {
+const UserInput = ({ values, handleChangeValues }) => {
+  let { initialInvestment, annualInvestment, expectedReturn, duration } =
+    values;
   return (
     <section id='user-input'>
       <div className='input-group'>
         <p>
           <label htmlFor='initialInvestment'>Initial Investment</label>
-          <input id='initialInvestment' type='number' value='' required />
+          <input
+            id='initialInvestment'
+            type='number'
+            value={initialInvestment}
+            onChange={handleChangeValues}
+            required
+          />
         </p>
         <p>
           <label htmlFor='annualInvestment'>Annual Investment</label>
-          <input id='annualInvestment' type='number' value='' required />
+          <input
+            id='annualInvestment'
+            type='number'
+            value={annualInvestment}
+            onChange={handleChangeValues}
+            required
+          />
         </p>
       </div>
       <div className='input-group'>
         <p>
           <label htmlFor='expectedReturn'>Expected Return</label>
-          <input id='expectedReturn' type='number' value='' required />
+          <input
+            id='expectedReturn'
+            type='number'
+            value={expectedReturn}
+            onChange={handleChangeValues}
+            required
+          />
         </p>
         <p>
           <label htmlFor='duration'>Duration</label>
-          <input id='duration' type='number' value='' required />
+          <input
+            id='duration'
+            type='number'
+            value={duration}
+            onChange={handleChangeValues}
+            required
+          />
         </p>
       </div>
     </section>
